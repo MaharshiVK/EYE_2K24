ce64b8e059c9e09968d678bee3b0d2a883eefabc
MaharshiVK <149076371+MaharshiVK@users.noreply.github.com>
2023-10-26 18:39:01 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+$email_form='eye2k24@website.com';
+$email_subject='New Form Submission';
+$email_body="User Name:$name.\n".
+            "User Email:$visitor_email.\n".
+             "User Name:$subject.\n".
+              "User Message:$message.\n";
+$to='rishikommu230@gmail.com';
+$headers="From: $email_from\r\n";
+$headers.="reply-To:$visitors_email\r\n";
+mail($to,$email_subject,$email_body,$headers);
+     header("Location:contact.html.html");
+?>
